ddb43cdc4753bca42ff76b540078864379603df0
Raheem Adebayo <adraheemzy@gmail.com>
2022-09-22 10:21:29 +0100

    Update pythagoreanTriple

--- src/classwork/chapter5/PythagoreanTriple.java
+++ src/classwork/chapter5/PythagoreanTriple.java
@@ -1,11 +1,13 @@
 package classwork.chapter5;
 
 public class PythagoreanTriple {
+    private static final int limitingValue = 20;
+
     public static void main(String[] args) {
-        System.out.printf("%5s%10s%10s%n", "Side1", "Side2", "Hypothenuse");
-        for (int i = 1; i <= 500; i++) {
-            for (int j = 1; j <= 500; j++) {
-                for (int hyp = 1; hyp <= 500; hyp++) {
+        System.out.printf("%5s%18s%20s%n", "Side1", "Side2", "Hypothenuse");
+        for (int i = 1; i <= limitingValue; i++) {
+            for (int j = 1; j <= limitingValue; j++) {
+                for (int hyp = 1; hyp <= limitingValue; hyp++) {
                     if(Math.pow(hyp, 2) == Math.pow(i, 2) + Math.pow(j, 2)){
                         System.out.printf("%5d       %10d       %10d%n", i, j, hyp);
                     }
