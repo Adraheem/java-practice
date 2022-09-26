1ee12c161654caa9483ab21ee81ec9fd29793ffd
Raheem Adebayo <adraheemzy@gmail.com>
2022-09-26 11:44:22 +0100

    Improve Global warming quiz

--- src/classwork/chapter5/GlobalWarmingQuiz.java
+++ src/classwork/chapter5/GlobalWarmingQuiz.java
@@ -9,14 +9,14 @@ public class GlobalWarmingQuiz {
         int correctAnswer = 0;
         int wrongAnswer = 0;
 
-        String answer = "";
+        String answer;
 
         for (int question = 1; question <= 5; question++) {
             System.out.println("\n\n"+ "=".repeat(80));
             System.out.printf("Question %d:%n", question);
 
-            switch(question){
-                case 1:
+            switch (question) {
+                case 1 -> {
                     System.out.println("What is Nigeria Independence Day?");
                     System.out.println("A: October 1st, 1960");
                     System.out.println("B: May 14, 1914");
@@ -24,13 +24,13 @@ public class GlobalWarmingQuiz {
                     System.out.println("D: December 25, 2000");
                     System.out.print("\nEnter your answer: ");
                     answer = input.nextLine();
-                    if(answer.trim().equalsIgnoreCase("a")){
+                    if (answer.trim().equalsIgnoreCase("a")) {
                         correctAnswer++;
                     } else {
                         wrongAnswer++;
                     }
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     System.out.println("Green is part of Nigeria color");
                     System.out.println("A: No");
                     System.out.println("B: Yes");
@@ -38,13 +38,13 @@ public class GlobalWarmingQuiz {
                     System.out.println("D: Not sure");
                     System.out.print("\nEnter your answer: ");
                     answer = input.nextLine();
-                    if(answer.trim().equalsIgnoreCase("b")){
+                    if (answer.trim().equalsIgnoreCase("b")) {
                         correctAnswer++;
                     } else {
                         wrongAnswer++;
                     }
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     System.out.println("White is part of Nigeria color");
                     System.out.println("A: No");
                     System.out.println("B: Yes");
@@ -52,13 +52,13 @@ public class GlobalWarmingQuiz {
                     System.out.println("D: Not sure");
                     System.out.print("\nEnter your answer: ");
                     answer = input.nextLine();
-                    if(answer.trim().equalsIgnoreCase("b")){
+                    if (answer.trim().equalsIgnoreCase("b")) {
                         correctAnswer++;
                     } else {
                         wrongAnswer++;
                     }
-                    break;
-                case 4:
+                }
+                case 4 -> {
                     System.out.println("How many states exist in Nigeria?");
                     System.out.println("A: 20");
                     System.out.println("B: 35");
@@ -66,13 +66,13 @@ public class GlobalWarmingQuiz {
                     System.out.println("D: Not sure");
                     System.out.print("\nEnter your answer: ");
                     answer = input.nextLine();
-                    if(answer.trim().equalsIgnoreCase("c")){
+                    if (answer.trim().equalsIgnoreCase("c")) {
                         correctAnswer++;
                     } else {
                         wrongAnswer++;
                     }
-                    break;
-                case 5:
+                }
+                case 5 -> {
                     System.out.println("What continent is Nigeria in?");
                     System.out.println("A: Asia");
                     System.out.println("B: Australia");
@@ -80,27 +80,21 @@ public class GlobalWarmingQuiz {
                     System.out.println("D: Africa");
                     System.out.print("\nEnter your answer: ");
                     answer = input.nextLine();
-                    if(answer.trim().equalsIgnoreCase("d")){
+                    if (answer.trim().equalsIgnoreCase("d")) {
                         correctAnswer++;
                     } else {
                         wrongAnswer++;
                     }
-                    break;
+                }
             }
         }
 
         System.out.println();
 
-        switch(correctAnswer){
-            case 5:
-                System.out.println("Excellent");
-                break;
-            case 4:
-                System.out.println("Very good");
-                break;
-            default:
-                System.out.println("Time to brush up on your knowledge of global warming");
-                break;
+        switch (correctAnswer) {
+            case 5 -> System.out.println("Excellent");
+            case 4 -> System.out.println("Very good");
+            default -> System.out.println("Time to brush up on your knowledge of global warming");
         }
 
         System.out.printf("%nYou got %d correctly and %d wrongly%n", correctAnswer, wrongAnswer);
