c767ead7935d44d504aba13bc3e8b557dd7cfe9f
Raheem Adebayo <adraheemzy@gmail.com>
2022-11-01 09:44:15 +0100

    sudoku numbers

---
+++ src/snacks/Sudoku.java
@@ -0,0 +1,20 @@
+package snacks;
+
+import java.util.Arrays;
+
+public class Sudoku {
+    public static void main(String[] args) {
+        int[][] sudoku = new int[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                int value = i + j + 1;
+                sudoku[i][j] = value > 9 ? value % 10 + 1 : value;
+            }
+        }
+
+        for(int[] row : sudoku){
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
