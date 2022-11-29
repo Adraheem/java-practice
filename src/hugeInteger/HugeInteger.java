8683c8c8ace3979f5fdfab41a62d30335709e67a
Raheem Adebayo <adraheemzy@gmail.com>
2022-11-29 09:44:45 +0100

    HugeInteger Completed

--- src/hugeInteger/HugeInteger.java
+++ src/hugeInteger/HugeInteger.java
@@ -2,14 +2,17 @@ package hugeInteger;
 
 import java.util.Arrays;
 
-public class HugeInteger {
+public class HugeInteger implements Comparable<HugeInteger> {
     private final int MAX_LIMIT = 40;
-    private int[] digits = new int[MAX_LIMIT];
+    private final int[] digits = new int[MAX_LIMIT];
+    private final boolean isNegative;
     public final int length;
+    public static HugeInteger ZERO = HugeInteger.parse("0");
 
 //    CONSTRUCTORS *************************************************************************
 
     public HugeInteger(String number) {
+        this.isNegative = number.contains("-");
         String[] digitString = this.trimZeros(number.replaceAll("\\D", "")).split("");
         length = digitString.length;
 
@@ -44,6 +47,10 @@ public class HugeInteger {
     public String toString() {
         StringBuilder value = new StringBuilder();
 
+        if (this.isNegative){
+            value.append("-");
+        }
+
         for (int i = (MAX_LIMIT - length + 1); i <= MAX_LIMIT; i++) {
             value.append(digits[i - 1]);
         }
@@ -56,7 +63,32 @@ public class HugeInteger {
         if (this.getClass() != obj.getClass()) return false;
 
         HugeInteger num2 = (HugeInteger) obj;
-        return Arrays.equals(this.digits, num2.digits);
+        return Arrays.equals(this.digits, num2.digits) && this.isNegative == num2.isNegative;
+    }
+
+    @Override
+    public int compareTo(HugeInteger num2){
+        if (this.equals(num2)) return 0;
+
+        int remainder = 0;
+        int counter = MAX_LIMIT - 1;
+
+
+        while (counter > Math.min((MAX_LIMIT - this.length), (MAX_LIMIT - num2.length)) - 1 ){
+            int d1 = this.digits[counter];
+            int d2 = num2.digits[counter];
+
+            int sub = d1 - remainder - d2;
+            remainder = sub < 0 ? 1 : 0;
+
+            counter--;
+        }
+
+        if (remainder == 1){
+            return -1;
+        } else {
+            return 1;
+        }
     }
 
 //    PRIVATE METHODS *************************************************************************
@@ -64,7 +96,7 @@ public class HugeInteger {
     private String trimZeros(String string){
         String value = string;
 
-        for (int i = 0; i < string.length(); i++){
+        for (int i = 0; i < string.length() - 1; i++){
             if(value.charAt(0) == '0'){
                 value = value.substring(1);
             } else {
@@ -73,6 +105,26 @@ public class HugeInteger {
         }
 
         return value;
+    }
+
+    private HugeInteger privateSubtract(HugeInteger num2, boolean isNegative) {
+        int remainder = 0;
+        int counter = MAX_LIMIT - 1;
+
+        StringBuilder minusString = new StringBuilder();
+
+        while (counter > 0 ){
+            int d1 = this.digits[counter];
+            int d2 = num2.digits[counter];
+
+            int sub = d1 - remainder - d2;
+            remainder = sub < 0 ? 1 : 0;
+
+            minusString.insert(0, (sub < 0 ? (10 + sub) : sub));
+            counter--;
+        }
+
+        return new HugeInteger((isNegative ? "-" : "") + minusString);
     }
 
 //    PUBLIC METHODS *************************************************************************
@@ -95,5 +147,33 @@ public class HugeInteger {
         }
 
         return new HugeInteger(sumString.toString());
+    }
+
+    public HugeInteger subtract(HugeInteger num2) {
+        if (this.compareTo(num2) < 0){
+            return num2.privateSubtract(this, true);
+        }
+
+        return this.privateSubtract(num2, false);
+    }
+
+    public boolean isGreaterThan(HugeInteger num2) {
+        return this.compareTo(num2) > 0;
+    }
+
+    public boolean isGreaterThanOrEqualTo(HugeInteger num2) {
+        return this.compareTo(num2) >= 0;
+    }
+
+    public boolean isEqualTo(HugeInteger num2) {
+        return this.compareTo(num2) == 0;
+    }
+
+    public boolean isLesserThan(HugeInteger num2) {
+        return this.compareTo(num2) < 0;
+    }
+
+    public boolean isLesserThanOrEqualTo(HugeInteger num2) {
+        return this.compareTo(num2) <= 0;
     }
 }
--- test/hugeInteger/HugeIntegerTest.java
+++ test/hugeInteger/HugeIntegerTest.java
@@ -24,6 +24,13 @@ public class HugeIntegerTest {
     }
 
     @Test
+    void numberCanBeNegativeTest() {
+        HugeInteger num1 = new HugeInteger(-500);
+
+        assertEquals("-500", num1.toString());
+    }
+
+    @Test
     void canCreateHugeIntegerWithStaticMethodTest() {
         HugeInteger num1 = HugeInteger.parse("12345");
         assertEquals("12345", num1.toString());
@@ -43,5 +50,71 @@ public class HugeIntegerTest {
         HugeInteger num2 = HugeInteger.parse("352166");
 
         assertEquals(HugeInteger.parse("494720019"), num1.add(num2));
+    }
+
+    @Test
+    void canSubtractOneNumberFromAnotherNumberTest() {
+        HugeInteger num1 = HugeInteger.parse(78532);
+        HugeInteger num2 = HugeInteger.parse("45265");
+
+        assertEquals(HugeInteger.parse("33267"), num1.subtract(num2));
+    }
+
+    @Test
+    void canSubtractNumberToGiveNegativeValueTest() {
+        HugeInteger num1 = new HugeInteger("43");
+        HugeInteger num2 = new HugeInteger("72");
+
+        assertEquals(HugeInteger.parse("-29"), num1.subtract(num2));
+    }
+
+    @Test
+    void numberIsGreaterTest() {
+        HugeInteger num1 = new HugeInteger("993");
+        HugeInteger num2 = new HugeInteger("72");
+
+        assertTrue(num1.isGreaterThan(num2));
+    }
+
+    @Test
+    void numberIsGreaterThanOrEqualToTest() {
+        HugeInteger num1 = new HugeInteger("72");
+        HugeInteger num2 = new HugeInteger("72");
+
+        assertTrue(num1.isGreaterThanOrEqualTo(num2));
+    }
+
+    @Test
+    void numberIsEqualToTest() {
+        HugeInteger num1 = new HugeInteger("67");
+        HugeInteger num2 = new HugeInteger("67");
+
+        assertTrue(num1.isEqualTo(num2));
+    }
+
+    @Test
+    void numberIsLesserThanTest() {
+        HugeInteger num1 = new HugeInteger("43");
+        HugeInteger num2 = new HugeInteger("72");
+
+        assertTrue(num1.isLesserThan(num2));
+    }
+
+    @Test
+    void numberIsLesserThanOrEqualToTest() {
+        HugeInteger num1 = new HugeInteger("43");
+        HugeInteger num2 = new HugeInteger("72");
+
+        assertTrue(num1.isLesserThanOrEqualTo(num2));
+    }
+
+    @Test
+    void hugeIntegerCanBeZeroTest() {
+        HugeInteger num1 = HugeInteger.parse(0);
+        HugeInteger num2 = new HugeInteger("00000");
+
+        assertEquals("0", num1.toString());
+        assertEquals("0", num2.toString());
+        assertEquals("0", HugeInteger.ZERO.toString());
     }
 }
